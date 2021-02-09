d1aae7cc08399f709bb39916c0d4cde77ab7a8a6
Neelam <neelamchaubey28@gmail.com>
2021-02-09 21:00:18 +0530

    added line through effect when player wins

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const restartBtn = document.querySelector('#restartBtn');
 const boxSpaces = [null, null, null, null, null, null, null, null, null];
 let currentPlayer = 'X';
 let playerTurn = document.querySelector('.playerTurn');
+const lineThrough = document.querySelector('.line-through');
+const container = document.querySelector('.container');
 
 playerTurn.innerHTML = `It's ${currentPlayer} 's turn`;
 
@@ -22,7 +24,6 @@ boxes.forEach(box => {
     if (box.id > 5) {
         box.style.borderTop = '2px solid  #676767'
     }
-
     // add current player's text in box and spaces array
     const addPlayerText = (e) => {
         if (boxSpaces[e.target.id] === null) {
@@ -39,12 +40,23 @@ boxes.forEach(box => {
         // top, left, diagnally from left
         if (boxSpaces[0] === currentPlayer) {
             if (boxSpaces[1] === currentPlayer && boxSpaces[2] === currentPlayer) {
+                lineThrough.style.display = 'block';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, up top!`;
             }
             if (boxSpaces[3] === currentPlayer && boxSpaces[6] === currentPlayer) {
+                lineThrough.style.display = 'block';
+                lineThrough.style.width = '100%';
+                lineThrough.style.left = '-100px';
+                lineThrough.style.top = '150px';
+                lineThrough.style.transform = 'rotate(90deg)';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, on the left!`;
             }
             if (boxSpaces[4] === currentPlayer && boxSpaces[8] === currentPlayer) {
+                lineThrough.style.display = 'block';
+                lineThrough.style.width = '130%';
+                lineThrough.style.left = '-48px';
+                lineThrough.style.top = '145px';
+                lineThrough.style.transform = 'rotate(45deg)';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, from the left, diagnally!`;;
             }
         }
@@ -52,9 +64,16 @@ boxes.forEach(box => {
         // right, bottom
         if (boxSpaces[8] === currentPlayer) {
             if (boxSpaces[7] === currentPlayer && boxSpaces[6] === currentPlayer) {
+                lineThrough.style.display = 'block';
+                lineThrough.style.left = '0px';
+                lineThrough.style.top = '250px';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, at the bottom!`;
             }
             if (boxSpaces[5] === currentPlayer && boxSpaces[2] === currentPlayer) {
+                lineThrough.style.display = 'block';
+                lineThrough.style.left = '100px';
+                lineThrough.style.transform = 'rotate(90deg)';
+                lineThrough.style.top = '150px';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, on the right!`;
             }
         }
@@ -62,20 +81,31 @@ boxes.forEach(box => {
         // middle vertical, middle horizontal, diagnally right
         if (boxSpaces[4] === currentPlayer) {
             if (boxSpaces[3] === currentPlayer && boxSpaces[5] === currentPlayer) {
+                lineThrough.style.display = 'block';
+                lineThrough.style.left = '0px';
+                lineThrough.style.top = '150px';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, in the middle, horizontally!`;
             }
             if (boxSpaces[1] === currentPlayer && boxSpaces[7] === currentPlayer) {
+                lineThrough.style.display = 'block';
+                lineThrough.style.left = '0px';
+                lineThrough.style.top = '150px';
+                lineThrough.style.transform = 'rotate(90deg)';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, in the middle, vertically!`;
             }
             if (boxSpaces[2] === currentPlayer && boxSpaces[6] === currentPlayer) {
+                lineThrough.style.display = 'block';
+                lineThrough.style.width = '130%';
+                lineThrough.style.left = '-40px';
+                lineThrough.style.top = '145px';
+                lineThrough.style.transform = 'rotate(-45deg)';
                 return `<span id="playerColor">${currentPlayer}</span> won the game, from the right, diagnally!`;;
             }
         }
     }
-
     box.addEventListener('click', (e) => {
         addPlayerText(e);
-
+        // add hover effect if there is content inside box
         box.addEventListener("mouseover", (e) => {
             box.style.cursor = 'no-drop';
         })
@@ -88,18 +118,27 @@ boxes.forEach(box => {
     })
 })
 
+
+
 const restartGame = () => {
+    // clear innerHtml of all div boxes
+    boxes.forEach(boxDiv => {
+        boxDiv.innerHTML = '';
+        // replace no-drop hover effect with pointer for all boxes
+        boxDiv.addEventListener("mouseover", (e) => {
+            boxDiv.style.cursor = 'pointer';
+        })
+    })
     // make all array elements null
     boxSpaces.forEach((box, index) => {
         boxSpaces[index] = null;
     })
 
-    // clear innerHtml of all div boxes
-    boxes.forEach(boxDiv => {
-        boxDiv.innerHTML = '';
-    })
+
     currentPlayerText.innerHTML = `Let's Play!`;
     currentPlayer = 'X';
+    lineThrough.style.display = 'none';
 }
 // reset the game
 restartBtn.addEventListener('click', restartGame);
+
